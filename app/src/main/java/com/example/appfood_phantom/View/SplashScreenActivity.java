03c40128d576390c2751bff37a7d0b3383a0ce65
hungto228 <hungto228@gmail.com>
2020-07-24 17:04:45 +0700

    create app and login

---
+++ app/src/main/java/com/example/appfood_phantom/View/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package com.example.appfood_phantom.View;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.example.appfood_phantom.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    //initilazation
+    TextView mVersion, mCompany, mLoading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        mVersion = findViewById(R.id.tv_version);
+        mCompany = findViewById(R.id.tv_nameCompany);
+        mLoading = findViewById(R.id.tv_loading);
+
+        //version name
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            mVersion.setText( getString(R.string.version)+" "+ packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
